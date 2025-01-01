4c6bf7481869a38a9491a9b42d2c48021264efdc
Jeroen Gerits <jeroen@gerits.email>
2025-01-01 23:45:16 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export { Card } from './Card';
-export { CardHeader } from './Card.Header';
-export { CardFooter } from './Card.Footer';
-export { CardTitle } from './Card.Title';
-export { CardDescription } from './Card.Description';
-export { CardContent } from './Card.Content';
-export type * from './types';
+console.log('card');
